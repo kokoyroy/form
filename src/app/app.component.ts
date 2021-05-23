3006aa1393b5998856c76bf7e9b860bd20b7a6e1
kokoyroy <kokoyroy@gmail.com>
2021-05-23 21:04:54 +0300

    form almost complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  topics = ['angular', 'react', 'vue'];
-
-  user = {
+  show = false;
+  person: User = {
     name: '',
+    dateOfBirth: '',
     email: '',
-    tel: '',
-    interest: '',
-    dateOfBirth: ''
+    tel: []
   };
 
+  submit(formdata: User): void {
+    this.show = true;
+    const tel = formdata.tel;
+    this.person.name = formdata.name;
+    this.person.tel.push(tel);
+    this.person.email = formdata.email;
+    this.person.dateOfBirth = formdata.dateOfBirth;
+    console.log(JSON.stringify(this.person));
+  }
+}
+
+export interface User {
+  name: string;
+  email: string;
+  // i had an issue of the tel type because if i let it string[] i had problem sto i set it to any...
+  // it is bad practice and i am aware of that but i am out of time this moment
+  tel: any[];
+  dateOfBirth: string;
 }
